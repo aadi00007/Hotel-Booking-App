9aeb762c12278bebcc50abaf5f6f433db235e5ec
Aditya <vasishtaditya8@gmail.com>
2025-07-13 15:55:44 +0530

    nkdk

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ import Home from "./Pages/Home";
 import Footer from "./components/Footer";
 import AllRooms from "./Pages/AllRooms";
 import RoomDetails from "./Pages/RoomDetails";
+import MyBookings from "./Pages/MyBookings";
+import HotelReg from "./components/HotelReg";
+import Layout from "./Pages/HotelOwner/Layout";
 const App =()=>{
   const isOwnerPath = useLocation().pathname.includes("owner");
   return(
     <div>
       {!isOwnerPath && <Navbar />}
+     {false && <HotelReg/>}
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/rooms' element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails/>}/>
+          <Route path="/my-bookings" element={<MyBookings/>}/>
+          <Route path="/owner" element={<Layout/>}>
+
+          </Route>
         </Routes>
 
       </div>
